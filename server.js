5deafa9099d5a237135e95f1a5284982ce023c1d
GW <garykachunwu@gmail.com>
2024-04-28 21:55:17 -0700

    finished code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const api = require('./public/assets/js/index.js');
+const api = require('./public/routes/index.js');
 
 const PORT = process.env.port || 3001;
 
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 
 //get route for notes
 app.get('/notes',(req,res) =>
-    res.sendFile(path.join(__dirname,'./public/note.html'))
+    res.sendFile(path.join(__dirname,'./public/notes.html'))
 );
 
 //get route for home page
@@ -22,5 +22,8 @@ app.get('*',(req,res) =>
     res.sendFile(path.join(__dirname,'./public/index.html'))
 );
 
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
 
 
